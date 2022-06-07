e097ab7f1229cc8e231b27061a0f9f9b8881e2d0
mpfuns <monetpaul23@gmail.com>
2022-06-07 18:07:25 -0400

    it works

--- script.js
+++ script.js
@@ -1,113 +1,139 @@
+class Calculator{
+    constructor(previousOperandText,currentOperandText){
+        this.previousOperandText= previousOperandText;
+        this.currentOperandText= currentOperandText;
+        this.clear();
+    }
+    clear(){
+        this.currentOperand= "";
+        this.previousOperand ="";
+        this.operation =undefined;
 
-//the function to do the math 
-
-const add = function(...nums) {
-    return nums.reduce((total, num) => total +num,0)
-  };
-
-  const subtract = function(...nums) {
-    return nums.reduce((total, num) => total - num,0)
- };
-
- const multiply = function(...nums) {
-    return nums.reduce((total, num) => total * num,0);
- };
-
- const divide = function(...nums) {
-    return nums.reduce((total, num) => total / num,0);
- };
- 
-// to match  the input and send them to the right function for math 
- const addState=/\d*(\.\d+)?\s\+\s\d*(\.\d+)?/gm;
-const subtractState= /\d*(\.\d+)?\s\-\s\d*(\.\d+)?/gm;
-const multiplyState=/\d*(\.\d+)?\s\*\s\d*(\.\d+)?/gm;
-const divideState=/\d*(\.\d+)?\s\/\s\d*(\.\d+)?/gm;
- 
-
-  function pickOperator (input){
-     
-    return input.match(addState)? add(input): 
-     input.match(subtractState)?subtract(input):
-     
-     input.match(multiplyState)?multiply(input):
-     
-     input.match(divideState)?divide(input):
-     
-     console.log("this didn't work");
-    };
-
-    function middleAnswer(str){
-        const mid= str.length/2;
-        console.log(mid);
-        const wholeNum= Math.floor(mid);
-        console.log(wholeNum);
-        return mid== wholeNum? mid: wholeNum+1;
     }
     
+    undo(){
+        this.currentOperand= this.currentOperand.toString().slice(0,-1)
+
+    }
     
-    function breakProblem (str){
-        
-        const middlePoint= middleAnswer(str);
-        console.log(middlePoint);
-        if(str.indexOf("+")== middlePoint){
-            const addMid= str.split("+");
-            console.log(addMid);
-        } 
-        else if(str.indexOf("-")== middlePoint){
-            const subMid= str.split("-");
-            console.log(subMid);
+    appendNumber (number){
+         if(number==='.' && this.currentOperand.includes('.')) return
+        this.currentOperand= this.currentOperand.toString()+ number.toString()
+    }
+    
+    ///
+    chooseOperation (operation){
+        if (this.currentOperand === '') return
+        if ( this.previousOperand !==''){
+            this.compute()
         }
-        else if(str.indexOf("*")== middlePoint){
-            const timeMid= str.split("*");
-            console.log(timeMid);
-        } 
-        else if(str.indexOf("/")== middlePoint){
-            const divideMid= str.split("/");
-            console.log(divideMid);
-        } 
-        else{
-            console.log("this didn't work")
-        }
+        this.operation =operation
+        this.previousOperand= this.currentOperand
+        this.currentOperand=""
     }
+    
+    compute(){
+        let computation 
+        const prev = parseFloat(this.previousOperand) 
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev)|| isNaN(current)) return
 
-
-    const inputText = document.getElementById("input");
-    
-     function undoButton () { 
-        const inputstring =String(inputText.textContent);
-        
-        const inputundo= inputstring.slice(0,-1);
-        
-        return inputText.textContent=`${inputundo}`;
-    }
-   
-    
-    
-    
-    
-    function whatButton(buttonId){
-        return buttonId=="clear"? inputText.textContent="": 
-        
-        buttonId=="="? breakProblem(inputText.textContent):
-        
-        buttonId=="undo"? undoButton():
-        
-        inputText.textContent+=`${buttonId}`;
-
-    }
-
-        
-    // make the the buttons work 
-    const buttons =document.querySelectorAll(".button-style");
-    
-    buttons.forEach((button)=>{
-        const idName= button.getAttribute("id");
-         
-         button.addEventListener("click",()=>{
-             whatButton(idName);
-            }
+        switch(this.operation){
+            case '+': 
+                computation =prev +current
+                break
             
-        );
-       
+            case '-': 
+                computation =prev - current
+                break
+            case '*': 
+                computation =prev * current
+                break
 
+            case '÷': 
+                computation =prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand= computation
+        this.operation =undefined
+        this.previousOperand =""
+
+    }
+    getDisplayNumber(number){
+        const stringNumber =number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay 
+        if(isNaN(integerDigits)){
+            integerDisplay =""
+        } else {
+            integerDisplay= integerDigits.toLocaleString('en', {maximumFractionDigits:0})
+        }
+        if(decimalDigits != null){
+           return `${integerDisplay}.${decimalDigits}` 
+        } else {
+            return integerDisplay
+        }
+    }
+
+
+    updateDisplay (){
+        this.currentOperandText.innerText= this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandText.innerText= 
+            `${this.getDisplayNumber(this.previousOperand)}${this.operation}`
+        }
+        else{ 
+            this.previousOperandText.innerText=""
+        }
+        
+    }
+
+}
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equal]');
+const undoButton = document.querySelector('[data-undo]');
+const clearButton = document.querySelector('[data-clear]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
     })
+})
+
+operationButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalButton.addEventListener('click', button =>{
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+clearButton.addEventListener('click', button =>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+undoButton.addEventListener('click', button =>{
+    calculator.undo()
+    calculator.updateDisplay()
+})
